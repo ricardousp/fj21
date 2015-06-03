b9c1ad394034f7f44dcf7f15ed7cfea4003aeca4
damascenodiego <carlos.damasceno08@gmail.com>
2015-06-03 00:13:55 -0300

    connection factory example

---
+++ fj21/src/fj21/ConnectionFactory.java
@@ -0,0 +1,16 @@
+package fj21;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public static Connection getConnection() {
+		try {
+			return DriverManager.getConnection(
+					"jdbc:mysql://localhost/fj21", "root", "r00t");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
--- fj21/src/fj21/JDBCExemplo.java
+++ fj21/src/fj21/JDBCExemplo.java
@@ -6,8 +6,9 @@ import java.sql.SQLException;
 
 public class JDBCExemplo {
 	public static void main(String[] args) throws SQLException {
-		Connection conexao = DriverManager.getConnection(
-				"jdbc:mysql://localhost/fj21");
+//		Connection conexao = DriverManager.getConnection(
+//				"jdbc:mysql://localhost/fj21");
+		Connection conexao = ConnectionFactory.getConnection();
 		System.out.println("Conectado!");
 		conexao.close();
 	}
