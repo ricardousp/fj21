39db9dee52133fa01c9c68e4fdc03b2546a76cd7
damascenodiego <carlos.damasceno08@gmail.com>
2015-06-03 00:11:41 -0300

    first commit

---
+++ fj21/src/fj21/JDBCExemplo.java
@@ -0,0 +1,14 @@
+package fj21;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCExemplo {
+	public static void main(String[] args) throws SQLException {
+		Connection conexao = DriverManager.getConnection(
+				"jdbc:mysql://localhost/fj21");
+		System.out.println("Conectado!");
+		conexao.close();
+	}
+}
