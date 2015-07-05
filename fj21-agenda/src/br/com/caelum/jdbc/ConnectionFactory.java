e616f88596984233a1164edf447222d1d0bedaab
damascenodiego <carlos.damasceno08@gmail.com>
2015-07-05 10:26:35 -0300

    mysql exception fixed

--- fj21-agenda/src/br/com/caelum/jdbc/ConnectionFactory.java
+++ fj21-agenda/src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	public Connection getConnection() {
 		try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());// without htis line a mysql exception was being thrown 
 			return DriverManager.getConnection(
 					"jdbc:mysql://localhost/fj21", "root", "r00t");
 		} catch (SQLException e) {
